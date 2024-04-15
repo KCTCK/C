f8440f6a2c57aaf940364d0488e334397f84325c
KCTC <kctc@noreply.codeberg.org>
2024-04-15 04:30:57 +0000

    上传文件至 a

---
+++ a/Ali.js
@@ -0,0 +1,60 @@
+[    
+    {
+        "name" : "小雅",
+        "server" : "http://alist.xiaoya.pro",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+  
+    {
+       
+        "name": "触光",
+        "server": "https://pan.ichuguang.com",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+    {
+        "name": "星梦",
+        "server": "https://pan.bashroot.top",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+        
+
+
+
+
+
+    {
+        "name": "一只鱼",
+        "server": "https://alist.youte.ml",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+    {
+        "name": "七米蓝",
+        "server": "https://al.chirmyram.com",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+
+    {
+        "name": "神族九帝",
+        "server": "https://alist.shenzjd.com",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    },
+    {
+        "name": "梓澪",
+        "server": "https://zi0.cc",
+        "startPage" : "/",
+        "showAll" : false,
+        "search" : true
+    }
+]
